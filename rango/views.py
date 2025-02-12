35b2d9339abf11efb5054830d543195942dcec3c
Thomas Keillor <2888969k@student.gla.ac.uk>
2025-02-12 14:53:21 +0000

    Chapter 10 upto p187 complete.

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from rango.forms import PageForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -17,8 +18,12 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
     
-    return render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
     context_dict = {'boldmessage':'This tutorial has been put together by Thomas'}
@@ -145,3 +150,18 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
